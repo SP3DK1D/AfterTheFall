885fde2654ab12ace1235d26e77c18fbcd6181ff
SP3DK1D <lukaslyster@gmail.com>
2024-12-19 10:21:21 -0700

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,14 @@
 package src;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player {
-    public static String location;//players location at all times
+    public static String location; // players location at all times
+    private static Map<String, Room> rooms = new HashMap<>(); // map to hold locations and Room objects
     
     public Player() {
-    Player player = new Player();//adds player object
+    Player player = new Player(); 
     Player.location = "prisonCell";//sets player starting location
     }
      
@@ -15,8 +19,12 @@ public class Player {
  */
     public void Movement(String direction) {
       direction = direction.toLowerCase();//sets the direction input to lower case to allow for differing inputs
-        if(direction.equals("north") || direction.equals("n")) {
-           if(){
+      Room current = rooms.get(location);//gets the current room
+      String[] temp = current.getNeighbors();//gives a connection between the directions and the rooms
+      
+       // if the player enters a valid direction it will move the player to the new location
+       if(direction.equals("north") || direction.equals("n")) {
+           if(!temp[0].equals("^")){
 
            }
            else{
@@ -24,26 +32,29 @@ public class Player {
            }
            
         } else if(direction.equals("south") || direction.equals("s")) {
-            if(){
-
+            if(!temp[1].equals("^")){
+                location = temp[1];
             }
             else{
                 System.out.println("You can't go that way, try another way.");
             }
         } else if(direction.equals("east") || direction.equals("e")) {
-            if(){
-
+            if(!temp[2].equals("^")){
+                location = temp[2];
             }
             else{
                 System.out.println("You can't go that way, try another way.");
             }
         } else if(direction.equals("west") || direction.equals("w")) {
-            if(){
-
+            if(!temp[3].equals("^")){
+                location = temp[3];
             }
             else{
                 System.out.println("You can't go that way, try another way.");
             }
+        } else { // if the player enters a direction that is not valid
+			System.out.println("Sorry, not valid direction, try again!");
+		}
         } 
     }
 }
