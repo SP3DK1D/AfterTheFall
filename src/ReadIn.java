3bbf56ae5c56297604d86f23480b2733e6fe589b
SP3DK1D <lukaslyster@gmail.com>
2024-12-18 21:35:01 -0700

    mess around with hash

--- src/ReadIn.java
+++ src/ReadIn.java
@@ -1,5 +1,50 @@
 package src;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.io.IOException;
+import java.util.*;
 
 public class ReadIn {
-    
+    public static HashMap<String, Room> createRooms() {
+
+		BufferedReader reader = new BufferedReader(new FileReader("Rooms.txt"));
+        String line = reader.readLine();
+        HashMap<String, Room> rooms = new HashMap<String, Room>();
+
+        while (line != null) { // while we can still read from file
+            String name = line;
+
+            line = reader.readLine();
+
+            String[] neighbors = line.split(",");
+            for (int i = 0; i < neighbors.length; i++) { // travers
+                                                            // neighbors
+                // trim each name
+                neighbors[i] = neighbors[i].trim();
+            }
+
+            line = reader.readLine();
+            String description = "";
+
+            while (!line.equals("END")) { // while reader has not hit "END"
+                // add each line to overall string for description
+                description = description + line + '\n';
+                line = reader.readLine();
+            }
+
+            // put new room object and rooms name in HashMap
+            rooms.put(name, new Room(name, description, neighbors, items));
+
+            line = reader.readLine(); // move line to beginning of next
+                                        // section of text
+                                        System.out.println(name);
+                                        System.out.println(description);
+                                        
+        }
 }
+public String getName() {
+    return name_;
+}
+}
+
--- src/Room.java
+++ src/Room.java
@@ -3,4 +3,14 @@ public class Room {
     private String roomName;
     private String[] roomNeighbors;
     private String RoomDescription;
+    private String name_; //name of room
+	private String[] neighbors_; //names of neighboring rooms
+	private String description_; //short description
+    public String getDescription() {
+        
+		return description_;
+	}
+    public String getName() {
+		return name_;
+	}
 }
