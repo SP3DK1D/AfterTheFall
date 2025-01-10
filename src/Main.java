05a023d5b48ea22e07d58c6702a8b5eb73900a91
SP3DK1D <lukaslyster@gmail.com>
2025-01-09 19:20:57 -0700

    s

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     // Scanner object to read user input
-    private static Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         // Print the main story start
